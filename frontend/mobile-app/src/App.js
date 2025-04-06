a75387848da10690cfd14c30bbc8567cec7a865b
suprachakra <111553143+suprachakra@users.noreply.github.com>
2025-04-06 13:44:43 +0800

    Create App.js

---
+++ frontend/mobile-app/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './screens/LoginScreen';
+import DashboardScreen from './screens/DashboardScreen';
+import GlobalErrorBoundary from './components/GlobalErrorBoundary';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <GlobalErrorBoundary>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GlobalErrorBoundary>
+  );
+}
